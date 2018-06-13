285169e78bb3113f47e70982b70b6f96c00034ae
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-13 22:26:34 +0000

    added layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,21 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  helpers do
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/users/login"
+      end
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
 end
