ea5f1012a8eda1663ef4538e9b5d26bbdf4e6d35
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-13 14:40:15 +0000

    added controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/' do
+    erb :index
+  end
 end
