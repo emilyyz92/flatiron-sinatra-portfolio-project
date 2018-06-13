a2722c2f84719ec1437dd9a4db058c1079c8871a
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-13 17:17:29 +0000

    added index view

--- db/migrate/20180613144632_create_users_table.rb
+++ db/migrate/20180613144632_create_users_table.rb
@@ -3,6 +3,8 @@ class CreateUsersTable < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+      t.string :email
+      t.string :name
     end
   end
 end
--- db/migrate/20180613151153_create_orders_table.rb.rb
+++ db/migrate/20180613151153_create_orders_table.rb.rb
@@ -1,7 +1,6 @@
 class CreateOrdersTable.rb < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.float :price
       t.integer :user_id
     end
   end
