ea2e18f301b1455bf80f8b12f81f9aac13b6b48a
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-14 14:09:21 +0000

    updated user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,29 @@ class UsersController < ApplicationController
       redirect "/users/signup"
     else
       @user = User.create(params[:user])
+      session[:user_id] = @user.id
       redirect "/users/#{@user.id}"
     end
   end
 
   get '/users/login' do
     erb :'/users/login'
+  end
+
+  post '/users/login' do
+    if params[:user].values.any?{|a|a == "" || a == nil}
+      flash[:error] = "Invalid user input, please try again."
+      redirect "/users/login"
+    else
+      user = User.find_by(username: params[:user][:username])
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        redirect "users/#{user.id}"
+      else
+        flash[:error] = "Invalid user input, please try again."
+        redirect "/users/login"
+      end
+    end
   end
 
   get '/users/:id' do
