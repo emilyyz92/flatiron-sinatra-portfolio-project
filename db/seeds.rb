777e10a3666c92d7ad9e8faf85997e8882f7600f
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-14 17:20:41 +0000

    minor update in view

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,6 @@ Order.create(user_id: 1)
 Category.create(name: "Gloves", price: 5.99, count: 100)
 Category.create(name: "Hats", price: 8.99, count: 60)
 Product.create(category_id: 1, order_id: 1)
+97.times do
+  Product.create(cateogry_id: 1)
+end
