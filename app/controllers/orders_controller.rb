28a0ed62d793281a0c7a5cba719504e01b581b76
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-15 17:02:18 +0000

    added create items method in order class

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,10 +8,7 @@ class OrdersController < ApplicationController
     if logged_in?
       @order = Order.create(user_id: current_user.id)
       array = params[:order][:product_id].zip(params[:order][:count])
-      array.map! do |a|
-        t = a[1].to_i
-        t.count.Item.create(product_id: a[0])
-      end
+      @order.create_items(array)
     end
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,22 @@
 class Order < ActiveRecord::Base
   belongs_to :user
-  has_many :products
-  has_many :categories, through: :products
+  has_many :items
+  has_many :products, through: :items
+
+  def create_items(array) #creating items when order created
+    array.each do |a|
+      t = a[1].to_i
+      item_array = []
+      t.times do
+        item = Item.create(product_id: a[0])
+        item_array << item
+      end
+    end
+      self.items = item_array
+      self.save
+  end
+
+  def price
+
+  end
 end
