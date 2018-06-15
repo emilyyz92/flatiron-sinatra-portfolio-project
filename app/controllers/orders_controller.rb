419bc914ba67071b818515e1867aa0d1199173ee
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-15 15:34:10 +0000

    updated post orderscontroller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,11 @@ class OrdersController < ApplicationController
     binding.pry
     if logged_in?
       @order = Order.create(user_id: current_user.id)
-      array = params[:order][:category_id].map! {|n|Cateogry.where(id: n)}
-      array.each {|category|@order.categories << category}
+      array = params[:order][:product_id].zip(params[:order][:count])
+      array.map! do |a|
+        t = a[1].to_i
+        t.count.Item.create(product_id: a[0])
+      end
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 User.create(username: "buyer1", password: "abc", name: "Jack", email: "jack@gmail.com")
 User.create(username: "panda", password: "kungfu", name: "Jackie", email: "jackie@gmail.com")
 Order.create(user_id: 1)
-Category.create(name: "Gloves", price: 5.99, count: 100)
-Category.create(name: "Hats", price: 8.99, count: 60)
-Product.create(category_id: 1, order_id: 1)
+Product.create(name: "Gloves", price: 5.99, count: 100)
+Product.create(name: "Hats", price: 8.99, count: 60)
 # Category.all.each do |c|
 #   c.count.times do
 #     Product.create(category_id: c.id)
