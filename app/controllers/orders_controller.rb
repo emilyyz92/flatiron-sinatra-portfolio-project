721c617a8fbc06a7f1782134505b3a95b01a7785
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-18 18:02:06 +0000

    added order delete view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,11 +16,39 @@ class OrdersController < ApplicationController
   get '/orders/:id' do
     @order = Order.find_by(id: params[:id])
     @products = @order.products
-    erb :'/orders/show'
+    if session[:user_id] == current_user.id
+      erb :'/orders/show'
+    else
+      flash[:message] = "You don't have access to this order"
+      redirect "/"
+    end
   end
 
   get '/orders/:id/edit' do
     @order = Order.find_by(id: params[:id])
     erb :'/orders/edit'
+  end
+
+  post '/orders/:id' do
+    @order = Order.find_by(id: params[:id])
+    @order.products.clear
+    Item.delete(@order.items)
+    @order.items.clear
+    array = params[:order][:product_id].zip(params[:order][:count])
+    @order.create_items(array)
+    @order.save
+    redirect "/orders/#{@order.id}"
+  end
+
+  get '/orders/:id/delete' do
+    if session[:user_id] == current_user.id
+      @order = Order.find_by(id: params[:id])
+      Item.delete(@order.items)
+      Order.delete(@order)
+      redirect "/users/#{session[:user_id]}"
+    else
+      flash[:message] = "You don't have access to this order"
+      redirect "/"
+    end
   end
 end
